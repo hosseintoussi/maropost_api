3567c83306e81f8095f36ae39c6e9e987c5dbef5
hosseintoussi <hossein@mindvalley.com>
2016-07-12 23:40:28 +0800

    start of more oo

--- lib/maropost_api.rb
+++ lib/maropost_api.rb
@@ -1,5 +1,9 @@
+require 'pry'
+
 require 'maropost_api/version'
+require 'maropost_api/request'
 require 'maropost_api/client'
+require 'maropost_api/contacts'
 
 module MaropostApi
 end
---
+++ lib/maropost_api/contacts.rb
@@ -0,0 +1,16 @@
+module MaropostApi
+  class Contacts
+    def initialize(request)
+      @request = request
+    end
+
+    def find_by_email(email:)
+      @request.get(endpoint: "contacts/email.json?contact[email]=#{email}")
+    end
+
+    def add_to_list(list_ids:, params:)
+      @list_ids = list_ids
+      @request.post(endpoint: "contacts.json?list_ids=#{@list_ids}", params: params)
+    end
+  end
+end
