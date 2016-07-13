9bdfdfba48a7a4515332a8260458bba41b2f0669
hosseintoussi <hossein@mindvalley.com>
2016-07-13 18:22:37 +0800

    added alias to update

--- lib/maropost_api/contacts.rb
+++ lib/maropost_api/contacts.rb
@@ -9,9 +9,10 @@ module MaropostApi
       Response.new(response: response).call
     end
 
-    def add_to_list(list_ids:, params:)
+    def add_to_list(list_ids: "", params:)
       response = @request.post(endpoint: "contacts.json?list_ids=#{list_ids}", params: params)
       Response.new(response: response).call
     end
+    alias_method :update, :add_to_list
   end
 end
