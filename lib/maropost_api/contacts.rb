75e8e7084916faadedfec222a2e6a7af37c185de
Haidar Muhammad <haidar.muhammad@gmail.com>
2016-07-20 11:36:04 +0800

    add response parsers class

--- lib/maropost_api/contacts.rb
+++ lib/maropost_api/contacts.rb
@@ -2,31 +2,32 @@ module MaropostApi
   class Contacts
     def initialize(request:)
       @request = request
+      @parser = Parser::EntityParser.new
     end
 
     def find_by_email(email:)
-      response = @request.get(endpoint: "contacts/email.json?contact[email]=#{email}")
-      Response.new(response: response).call
+      response = @request.get(endpoint: "/contacts/email.json?contact[email]=#{email}")
+      Response.new(response: response, parser: @parser).call
     end
 
     def add_to_list(list_ids:, params:)
-      response = @request.post(endpoint: "contacts.json?list_ids=#{list_ids}", params: params)
-      Response.new(response: response).call
+      response = @request.post(endpoint: "/contacts.json?list_ids=#{list_ids}", params: params)
+      Response.new(response: response, parser: @parser).call
     end
 
     def create(params:)
-      response = @request.post(endpoint: "contacts.json", params: params)
-      Response.new(response: response).call
+      response = @request.post(endpoint: "/contacts.json", params: params)
+      Response.new(response: response, parser: @parser).call
     end
 
     def update(contact_id:, params:)
-      response = @request.put(endpoint: "contacts/#{contact_id}.json", params: params)
-      Response.new(response: response).call
+      response = @request.put(endpoint: "/contacts/#{contact_id}.json", params: params)
+      Response.new(response: response, parser: @parser).call
     end
 
     def unsubscribe_all_lists(email:)
-      response = @request.put(endpoint: "contacts/unsubscribe_all.json?contact[email]=#{email}")
-      Response.new(response: response).call
+      response = @request.put(endpoint: "/contacts/unsubscribe_all.json?contact[email]=#{email}")
+      Response.new(response: response, parser: @parser).call
     end
   end
 end
