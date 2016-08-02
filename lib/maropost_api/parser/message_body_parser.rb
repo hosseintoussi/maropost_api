18cdf902e8e13de6e945d9c65ba0db43a43ce10c
Haidar Muhammad <haidar.muhammad@gmail.com>
2016-08-02 14:02:33 +0800

    remove unneeded parser

--- lib/maropost_api/parser/message_body_parser.rb
+++
@@ -1,9 +0,0 @@
-module MaropostApi
-  module Parser
-    class MessageBodyParser
-      def call(response)
-        Hashie::Mash.new(message: response.body)
-      end
-    end
-  end
-end
--- lib/maropost_api/workflows.rb
+++ lib/maropost_api/workflows.rb
@@ -2,18 +2,17 @@ module MaropostApi
   class Workflows
     def initialize(request:)
       @request = request
-      @body_parser = Parser::MessageBodyParser.new
       @entity_parser = Parser::EntityParser.new
     end
 
     def stop(workflow_id:, contact_id:)
       response = @request.put(endpoint: "/workflows/#{workflow_id}/stop/#{contact_id}.json")
-      Response.new(response: response, parser: @body_parser).call
+      Response.new(response: response, parser: @entity_parser).call
     end
 
     def start(workflow_id:, contact_id:)
       response = @request.put(endpoint: "/workflows/#{workflow_id}/start/#{contact_id}.json")
-      Response.new(response: response, parser: @body_parser).call
+      Response.new(response: response, parser: @entity_parser).call
     end
 
     def reset(workflow_id:, contact_id:)
